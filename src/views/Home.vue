b498a637daa13b8371ef146ef7b08aee0844ba53
Esdras <eesc.gg@gmail.com>
2020-07-24 22:39:02 -0600

    None

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -50,7 +50,7 @@
             <span>Git</span> &nbsp;
             <span>Linux</span>
           </p>
-          <a href='./res2020.pdf' target='_blank' class='moreLink'><span>View Resume</span><img src='../assets/navigationIcons/arrowRight.svg' width='20px' height='20px'></a>
+          <a href='./rec2020.pdf' target='_blank' class='moreLink'><span>View Resume</span><img src='../assets/navigationIcons/arrowRight.svg' width='20px' height='20px'></a>
         </div>
       </div>
     </div>
